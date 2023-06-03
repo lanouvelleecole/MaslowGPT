f63278624e08631bf10e3bb3c4ae5e39dad61a45
lanouvelleecole <la.nouvelle.ecole.2019@gmail.com>
2023-06-03 19:21:34 +0200

    Added RN rename list

--- src/AllGenerators/SetCreateReactNativeAppAsyncActions/ReplaceWordsInFolder/pieces/ReplaceWordsInFolderRequest.js
+++ src/AllGenerators/SetCreateReactNativeAppAsyncActions/ReplaceWordsInFolder/pieces/ReplaceWordsInFolderRequest.js
@@ -1,5 +1,8 @@
+import { readFilesRecursively } from "../../../../services/GetAllFilesFromPath/getAllFilesFromPath.js";
 import { GetChatGPTOutput } from "../../../../services/GetChatGPTOutput/GetChatGPTOutput.js";
+import { getPathWithForwardSlashes } from "../../../../services/GetPathWithForwardSlashes/getPathWithForwardSlashes.js";
 import { MakeLoadingSpinner } from "../../../../services/MakeLoadingSpinner/MakeLoadingSpinner.js";
+import path from "path";
 
 async function ReplaceWordsInFolderRequest(
   answers,
@@ -8,8 +11,36 @@ async function ReplaceWordsInFolderRequest(
 ) {
   try {
     console.log("remplacage");
+
+    const repoPath = answers.repoPath;
+
+    if (repoPath != null) {
+      const allFilesInFolder = readFilesRecursively(
+        repoPath,
+        [
+          ".json",
+          ".md",
+          ".gradle",
+          ".java",
+          ".xml",
+          ".mm",
+          ".plist",
+          ".storyboard",
+          ".pbxproj",
+          ".xcscheme",
+          ".m",
+          "",
+        ],
+        [".git", "assets"]
+      ).map((somepath) =>
+        getPathWithForwardSlashes(path.join(appRootPath, somepath))
+      );
+
+      debugger;
+    }
   } catch (error) {
     throw new Error(`Failed to create RN app: ${error}`);
   }
 }
+
 export { ReplaceWordsInFolderRequest };
